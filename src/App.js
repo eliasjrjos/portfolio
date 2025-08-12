ea2cba93ba4f51e6d66893636de4a3ba3704bef3
eliasjrjos <eliasjrjos@gmail.com>
2025-08-12 17:25:02 +0200

    Replace BrowserRouter by HashRouter in index.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/homepage/Home';
 import About from './pages/about/About';
